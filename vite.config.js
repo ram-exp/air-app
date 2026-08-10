aaa2721ba3c73a32c83023d36603cf4712b0d327
ramm01 <142164132+achmadramadhan1@users.noreply.github.com>
2026-08-10 21:43:24 +0700

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: './',
+  base: '/air-app/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
